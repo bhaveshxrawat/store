c9464085ab09fcc3148ca713ab4119c168ce5011
Bhavesh Rawat <rawatbhavesh2001@gmail.com>
2025-02-11 23:52:52 +0530

    initialised database and created a table

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import productRoutes from "./routes/productRoutes.js"
+import { sql } from "./config/db.js";
 
 dotenv.config();
 
@@ -18,4 +19,18 @@ app.use(morgan("dev")) //log the requests
 
 app.use("/api/products", productRoutes)
 
-app.listen(PORT)
+async function initDB() {
+  try {
+    await sql`CREATE TABLE IF NOT EXISTS products (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      image VARCHAR(255) NOT NULL,
+      price DECIMAL(10, 2) NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )`
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+}
+
+initDB().then(() => app.listen(PORT))
