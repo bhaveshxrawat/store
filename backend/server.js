c5a4b176a8648be0d95fd18ccc602e776f85e7ea
Bhavesh Rawat <rawatbhavesh2001@gmail.com>
2025-02-12 19:10:48 +0530

    set up arcjet for rate limiting and bot detection

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import dotenv from "dotenv";
 
 import productRoutes from "./routes/productRoutes.js"
 import { sql } from "./config/db.js";
+import { aj } from "./lib/arcjet.js";
 
 dotenv.config();
 
@@ -16,6 +17,44 @@ app.use(express.json())
 app.use(cors())
 app.use(helmet()) //security middleware for setting http headers
 app.use(morgan("dev")) //log the requests
+
+// apply arcjet rate-limit to all route
+app.use(async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req, {
+      requested: 1
+    })
+    if (decision.isDenied()) {
+      switch (true) {
+        case decision.reason.isRateLimit():
+          res.status(429).json({
+            error: "Too Many Requests"
+          });
+          break;
+        case decision.reason.isBot():
+          res.status(403).json({
+            error: "Bot access denied"
+          });
+          break;
+        default:
+          res.status(403).json({
+            error: "Forbidden"
+          });
+      }
+      return;
+    }
+
+    if (decision.results.some((result) => result.reason.isBot() && result.reason.isSpoofed())) {
+      res.status(403).json({ error: "Spoofed bot detected" });
+      return;
+    }
+    next()
+  }
+  catch (e) {
+    console.log("Arcjet error", e);
+    next(error)
+  }
+})
 
 app.use("/api/products", productRoutes)
 
